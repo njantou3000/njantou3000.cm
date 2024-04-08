fa7c8e43fde26cb31f87d8279c420d620dd038fd
prince-arthur <166257854+njantou3000@users.noreply.github.com>
2024-04-08 14:57:33 +0100

    Add files via upload

---
+++ ACCUEIL.js
@@ -0,0 +1,17 @@
+alert('Hello world !');
+console.log('Hello word !');
+ let dividende;
+ let diviseur;
+ quotient=0;
+
+
+
+ console.log(diviseur);
+ dividende= Number.parseFloat(prompt("Entrer le dividende","0"));
+ diviseur=Number.parseFloat(prompt("Entrer le diviseur","1"));
+quotient=dividende/diviseur;
+
+
+
+
+alert("Le quotient de la division est"+quotient+".")
